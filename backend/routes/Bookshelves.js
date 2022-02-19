4a0c76689519b444c9c6e68661388fad7fb1222f
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-19 20:31:45 +0100

    delete bookshelf method added

--- backend/routes/Bookshelves.js
+++ backend/routes/Bookshelves.js
@@ -1,20 +1,34 @@
 const express = require("express");
 const router = express.Router();
-const { BOOKSHELF } = require("../models");
+const { BOOKSHELF, BOOK } = require("../models");
 
-//sends all bookshelves to the frontend
+//get all bookshelves
 router.get("/getallbookshelves", async (req, res) =>{
     const allBookshelfes = await BOOKSHELF .findAll();
     res.json(allBookshelfes);
 
 })
 
-//adds a new bookshelf to the database
+//creates new bookshelf
 router.post("/newbookshelf", async (req, res) => {
     const newbookshelf = req.body;
-    await BOOKSHELF .create(newbookshelf);
+    await BOOKSHELF.create(newbookshelf);
     res.json(newbookshelf);
   });
+
+//delete bookshelf
+
+router.delete("/deleteshelf/:id", async (req, res) => {
+  const ID_BOOKSHELF = req.params.id
+  console.log(ID_BOOKSHELF);
+  try{
+    const deleteShelf = await BOOKSHELF.findOne({where: {ID_BOOKSHELF}});
+    await deleteShelf.destroy();
+    return res.json(deleteShelf);
+  } catch (err){
+    console.log(err);
+  }
+})
 
 
 module.exports = router;
