e10bac2dd8481e9493d11ea075cb40639b9f574a
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-20 16:55:38 +0100

    FrontpageNavbar created

---
+++ frontend/src/components/navbar/NavbarFrontpage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <a href="/">Login</a>
+      <a href="/register">Registrierung</a>
+    </div>
+  );
+}
+
+export default Navbar;
