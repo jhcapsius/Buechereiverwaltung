29a03ff40efc8efa48d52a2bed1067a28fbb61e7
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-22 02:24:22 +0100

    navigate to login when not logged in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Library from "./pages/Library/Library";
 import Register from "./pages/Register/Register"
 import Administration from "./pages/Adminstration/Administration";
@@ -7,15 +7,16 @@ import Login from "./pages/Login/Login";
 
 function App() {
   
+  let loggedIn = sessionStorage.getItem("accessToken") ? true : false;
 
   return (
     <>
       <BrowserRouter>
       <Routes>
-        <Route path="/library" element={<Library/>}/>
+        <Route path="/library" element={loggedIn ? <Library/> : <Navigate to="/" />}/>
         <Route path="/" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route path="/administration" element={<Administration/>}/>
+        <Route path="/administration" element={loggedIn ? <Administration/> : <Navigate to="/" />}/>
       </Routes>
       </BrowserRouter>
     </>
