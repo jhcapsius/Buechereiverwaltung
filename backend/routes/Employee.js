90914ce838d4c00669ebfebb3e93c86ef86f5b59
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-22 03:40:54 +0100

    if clauses updated

--- backend/routes/Employee.js
+++ backend/routes/Employee.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { EMPLOYEE } = require("../models");
 const { sign } = require("jsonwebtoken");
 
-router.post("/login",  async (req, res) => {
+router.post("/login", async (req, res) => {
   const { ID_EMPLOYEE, PASSWORD } = req.body;
   const JOB_TITLE = "Bibliothekar";
 
@@ -11,23 +11,26 @@ router.post("/login",  async (req, res) => {
 
   if (!employee) {
     res.json({ loggedIn: false, message: "ID nicht gefunden" });
-  }
-
-  if (JOB_TITLE === employee.JOB_TITLE) {
-    if (PASSWORD === employee.PASSWORD) {
-        const accessToken = sign({employee: employee.EMAIL_ADDRESS}, "~&Nc<SDtH}:uPjsW");
-        res.send({ loggedIn: true, accessToken, id: employee.ID_EMPLOYEE});
+  } else {
+    if (JOB_TITLE === employee.JOB_TITLE) {
+      if (PASSWORD === employee.PASSWORD) {
+        const accessToken = sign(
+          { employee: employee.EMAIL_ADDRESS },
+          "~&Nc<SDtH}:uPjsW"
+        );
+        res.send({ loggedIn: true, accessToken, id: employee.ID_EMPLOYEE });
+      } else {
+        res.json({
+          loggedIn: false,
+          message: "Ungültige Kombination aus ID und Passwort!",
+        });
+      }
     } else {
       res.json({
         loggedIn: false,
-        message: "Ungültige Kombination aus ID und Passwort!",
+        message: "Id hat nicht die benötigten Berechtigungen",
       });
     }
-  } else {
-    res.json({
-      loggedIn: false,
-      message: "Id hat nicht die benötigten Berechtigungen",
-    });
   }
 });
 
