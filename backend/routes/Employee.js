86e9a363444a88483daa5f080c3fab8679bfdd40
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-22 03:08:02 +0100

    login with authentication created

--- backend/routes/Employee.js
+++ backend/routes/Employee.js
@@ -1,33 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const { EMPLOYEE } = require("../models");
+const { sign } = require("jsonwebtoken");
 
-router.post("/login", async (req, res) =>{
-    const ID_EMPLOYEE = req.body.id;
-    const PASSWORD = req.body.password;
-    const JOB_TITLE = 'Bibliothekar';
+router.post("/login",  async (req, res) => {
+  const { ID_EMPLOYEE, PASSWORD } = req.body;
+  const JOB_TITLE = "Bibliothekar";
 
-    
-    try{
-        const employee = await EMPLOYEE.findOne({where: {ID_EMPLOYEE}});
+  const employee = await EMPLOYEE.findOne({ where: { ID_EMPLOYEE } });
 
-        if(employee != null){
-            if(employee.JOB_TITLE === JOB_TITLE){
-                if(employee.PASSWORD === PASSWORD){
-                    res.send({loggedIn: true})
-                }else{
-                    res.json({loggedIn: false, message: "Ungültige Kombination aus ID und Passwort!"})
-                }
-            }else{
-                res.json({loggedIn: false, message: "Id hat nicht die benötigten Berechtigungen"})
-            }
-        }else{
-            res.json({loggedIn: false, message: "ID nicht gefunden"})
-        }
-        
-    }catch(err){
-        console.log(err);
+  if (!employee) {
+    res.json({ loggedIn: false, message: "ID nicht gefunden" });
+  }
+
+  if (JOB_TITLE === employee.JOB_TITLE) {
+    if (PASSWORD === employee.PASSWORD) {
+        const accessToken = sign({employee: employee.EMAIL_ADDRESS}, "~&Nc<SDtH}:uPjsW");
+        res.send({ loggedIn: true, accessToken, id: employee.ID_EMPLOYEE});
+    } else {
+      res.json({
+        loggedIn: false,
+        message: "Ungültige Kombination aus ID und Passwort!",
+      });
     }
-})
+  } else {
+    res.json({
+      loggedIn: false,
+      message: "Id hat nicht die benötigten Berechtigungen",
+    });
+  }
+});
 
 module.exports = router;
