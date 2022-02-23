a27c9334d81d1ed1abd9dd074cff3b2bbeda16b2
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-23 05:51:16 +0100

    Database navbar created

---
+++ frontend/src/components/navbar/NavbarDatabase.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="Content-Container">
+      <div className="Content-Right">
+      <a href="/">Zurück zur Startseite</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
