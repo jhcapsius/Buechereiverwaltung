8135ac33493a2ef1976beeb5119b188992864fcc
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-23 09:22:53 +0100

    documentation added

--- backend/routes/Employee.js
+++ backend/routes/Employee.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const { EMPLOYEE } = require("../models");
 const { sign } = require("jsonwebtoken");
 
-router.get("/getallemployees", async (req, res) =>{
+//gets all employees
+router.get("/getallemployees", async (req, res) => {
   const allEmployees = await EMPLOYEE.findAll();
   res.json(allEmployees);
-})
+});
 
+//login route
 router.post("/login", async (req, res) => {
   const { ID_EMPLOYEE, PASSWORD } = req.body;
   const JOB_TITLE = "Bibliothekar";
 
   const employee = await EMPLOYEE.findOne({ where: { ID_EMPLOYEE } });
+  console.log(employee);
 
+  //login logic
   if (!employee) {
-    res.json({ loggedIn: false, message: "ID nicht gefunden" });
+    res.json({ loggedIn: false, message: "ID nicht gültig" });
   } else {
     if (JOB_TITLE === employee.JOB_TITLE) {
       if (PASSWORD === employee.PASSWORD) {
@@ -33,7 +37,7 @@ router.post("/login", async (req, res) => {
     } else {
       res.json({
         loggedIn: false,
-        message: "Id hat nicht die benötigten Berechtigungen",
+        message: "ID nicht gültig",
       });
     }
   }
