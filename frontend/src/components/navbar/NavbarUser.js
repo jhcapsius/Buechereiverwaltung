da7711eff94134ac5047416d23b6f8455e5cb853
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-23 09:34:59 +0100

    added library and profil route to navbar

--- frontend/src/components/navbar/NavbarUser.js
+++ frontend/src/components/navbar/NavbarUser.js
@@ -4,11 +4,17 @@ import "./Navbar.css";
 function Navbar() {
   const logout = () => {
     sessionStorage.clear();
-  }
+  };
 
   return (
-    <div className="Navbar">
-      <a onClick={logout} href="/">Logout</a>
+    <div className="Content-Container">
+      <div className="Content-Left">
+        <a href="/library">Bücherei</a>
+        <a href="/profile">Profil</a>
+        <a onClick={logout} href="/">
+          Logout
+        </a>
+      </div>
     </div>
   );
 }
