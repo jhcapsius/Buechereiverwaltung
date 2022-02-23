cf55dc28f1b31f9c7a9ae46bd707a8901a0d63b0
jhcapsius <89898992+jhcapsius@users.noreply.github.com>
2022-02-23 05:50:33 +0100

    get all jobtitles route added

--- backend/routes/Jobtitle.js
+++ backend/routes/Jobtitle.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { JOB_TITLE } = require("../models");
 
-router.get("/alljobtitles", async (req, res) =>{
+router.get("/getalljobtitles", async (req, res) =>{
     const allJobtitles = await JOB_TITLE.findAll();
     res.json(allJobtitles);
 })
